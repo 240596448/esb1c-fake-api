fd7648034cdef02e8c9463e119a4589b5ecb99d7
Vladimir Nadulich <g240596448@gmail.com>
2025-11-30 21:10:16 +0300

    поправил импорты

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,6 @@
 """Пакет приложения"""
+from app.main import app
+from app.storage import storage
+from app.routes import bp
 
+__all__ = ['app', 'storage', 'bp']
--- app/example.py
+++
@@ -1,8 +0,0 @@
-import os
-from esb1c import Application
-
-application = Application(
-    url=os.getenv("APPLICATION_URL"),
-    client_id=os.getenv("CLIENT_ID"),
-    client_secret=os.getenv("CLIENT_SECRET"),
-)
