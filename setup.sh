ec4a634d946c9bb1c341eba1870fc7c7bb2a6de8
Vladimir Nadulich <g240596448@gmail.com>
2025-12-07 03:28:41 +0300

    setup

--- setup.sh
+++ setup.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-# docker stop esb1c-fake-api 
-# docker rm esb1c-fake-api 
-# docker rmi esb1c-fake-api 
+container_name="esb1c-fake-api"
+image_name="2405964484/${container_name}"
+server_url="http://localhost:9090"
+
+docker stop ${container_name}
+docker rm ${container_name} 
+# docker rmi ${image_name}
 
 echo "Building Docker image..."
-docker build -t esb1c-fake-api .
+docker build -t ${image_name} .
 
 echo "Running Docker container..."
-docker run -d --name esb1c-fake-api -p 9090:5000 esb1c-fake-api
+docker run -d --name esb1c-fake-api -p 9090:5000 ${image_name}
 
-echo "Container started. Access the API at http://localhost:9090"
+echo "Container started. Access the API at ${server_url}"
 
-timeout 30 bash -c 'while ! curl -s http://localhost:9090 > /dev/null 2>&1; do sleep 1; done' || echo "Timeout: API not available"
+timeout 30 bash -c "while ! curl -s ${server_url} > /dev/null 2>&1; do sleep 1; done" || echo "Timeout: API not available"
 
 echo "Loading data..."
-python scripts/load_data.py http://localhost:9090
+python scripts/load_data.py ${server_url}
