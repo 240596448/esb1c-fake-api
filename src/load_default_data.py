6b30e6cba35c3bc7e7dba318d1f0d8a1994cbe8d
Vladimir Nadulich <g240596448@gmail.com>
2025-12-08 00:45:32 +0300

    Упростил загрузчик

--- src/load_default_data.py
+++ src/load_default_data.py
@@ -1,94 +1,17 @@
 """Скрипт для загрузки данных из JSON файлов"""
 
-import json
 import sys
 from pathlib import Path
-import requests
-
-data_dir = Path(__file__).parent.parent / "data" / "example-esb-answer"
-
-def load_token(url: str, client_id: str, client_secret: str):
-    """Загрузить данные токена из get_token.json"""
-    token_file = data_dir / "get_token.json"
-    token_file_data = json.loads(token_file.read_text(encoding="utf-8"))
-
-    response = requests.post(
-        f"{url}/setup/token",
-        auth=(client_id, client_secret),
-        json=token_file_data,
-        timeout=10,
-    )
-    response.raise_for_status()
-
-    print(f"Данные токена пользователя '{client_id}' успешно загружены")
-    return response.json()
-
-def load_metadata_channels(url: str, application_name: str, token: str):
-    """Загрузить metadata каналы из get_metadata_channels.json"""
-    metadata_file = data_dir / "get_metadata.json"
-    metadata = json.loads(metadata_file.read_text(encoding="utf-8"))
-
-    response = requests.post(
-        f"{url}/setup/metadata_channels",
-        json=metadata,
-        headers={
-            "Authorization": f"Bearer {token}", 
-            "x-fake-application-name": application_name
-            },
-        timeout=10,
-    )
-    response.raise_for_status()
-
-    print(f"Metadata каналы успешно загружены: {application_name}:{token}")
-    return response.json()
-
-
-def load_runtime_channels(url: str, application_name: str, token: str):
-    """Загрузить runtime каналы из get_runtime_channels.json"""
-    runtime_channels_file = data_dir / "get_runtime_channels.json"
-    runtime_channels = json.loads(runtime_channels_file.read_text(encoding="utf-8"))
-
-    response = requests.post(
-        f"{url}/setup/runtime_channels",
-        json=runtime_channels,
-        headers={
-            "Authorization": f"Bearer {token}", 
-            "x-fake-application-name": application_name
-        },
-        timeout=10,
-        )
-    response.raise_for_status()
-
-    print(f"Runtime каналы успешно загружены: {application_name}:{token}")
-    return response.json()
-
+from app.loader import MultiConfig, ApplicationLoader
 
 if __name__ == "__main__":
     base_url = sys.argv[1] if len(sys.argv) > 1 else "http://localhost:5000"
-    print(f"Загружаем данные в {base_url}")
+    arg_dir = sys.argv[2] if len(sys.argv) > 2 else "data/example-esb-answer"
+    data_dir = Path(__file__).parent / arg_dir
+    print(f"Загрузка данных в сервис по адресу {base_url} и директории {data_dir.absolute()}")
 
-    client_file = data_dir / "client.json"
-    if not client_file.exists():
-        print(f"Файл {client_file} не найден")
-        sys.exit(1)
-    try:
-        client_data = json.loads(client_file.read_text(encoding="utf-8"))
-    except Exception as e:
-        print(f"Ошибка загрузки данных из файла {client_file}: {e}")
-        sys.exit(1)
-
-    try:
-        token_data = load_token(
-            url=base_url,
-            client_id=client_data["client_id"],
-            client_secret=client_data["client_secret"])
-        metadata_channels_data = load_metadata_channels(
-            url=base_url,
-            application_name=client_data["application_name"],
-            token=token_data["value"]["id_token"])
-        runtime_channels_data = load_runtime_channels(url=base_url,
-            application_name=client_data["application_name"],
-            token=token_data["value"]["id_token"])
-    except Exception as e:
-        print(f"Ошибка загрузки данных: {e}")
-        sys.exit(1)
+    config = MultiConfig(data_dir)
+    loader = ApplicationLoader(config, base_url)
+    loader.load_token()
+    loader.load_metadata()
+    loader.load_runtime_channels()
