b302081b125f02afca27a3bac67d9dc22adb3079
Vladimir Nadulich <vladimir@nadulich.ru>
2025-12-11 23:51:08 +0300

    создание обменника и очереди раздельно

--- src/app/rabbitmq.py
+++ src/app/rabbitmq.py
@@ -50,6 +50,16 @@ class RabbitMQClient:
             self._connection.close()
         print("Соединение с RabbitMQ закрыто")
     
+    def create_exchange(self, exchange_name: str, exchange_type: str, durable: bool = True):
+        """
+        Создать exchange в RabbitMQ
+        """
+        try:
+            self._channel.exchange_declare(exchange=exchange_name, exchange_type=exchange_type, durable=durable)
+            print(f"Exchange создан: {exchange_name}")
+        except (pika.exceptions.AMQPConnectionError, pika.exceptions.AMQPChannelError) as e:
+            print(f"Ошибка создания exchange {exchange_name}: {e}")
+            
     def create_queue(self, queue_name: str, durable: bool = True):
         """
         Создать очередь в RabbitMQ
@@ -59,7 +69,6 @@ class RabbitMQClient:
             print(f"Очередь создана: {queue_name}")
         except (pika.exceptions.AMQPConnectionError, pika.exceptions.AMQPChannelError) as e:
             print(f"Ошибка создания очереди {queue_name}: {e}")
-            raise ValueError(f"Ошибка создания очереди {queue_name}: {e}")
 
     def __enter__(self):
         """Контекстный менеджер: вход"""
@@ -78,18 +87,31 @@ def create_queues(runtime_channels: dict, host: str, port: int, token: str):
     items = runtime_channels.get('items', [])
     if not items:
         raise ValueError(f"runtime_channels.items - список каналов пуст")
+
+    exchange_names = []
     queue_names = []
     for item in items:
         if isinstance(item, dict) and 'destination' in item:
             destination = item['destination']
-            if destination:
+            if not destination:
+                continue
+            elif destination.startswith('/exchanges/'):
+                exchange_name = destination.split('/')[2]
+                exchange_names.append(exchange_name)
+            elif destination.startswith('/queues/'):
+                queue_name = destination.split('/')[2]
+                queue_names.append(queue_name)
+            else:
                 queue_names.append(destination)
-    if not queue_names:
-        raise ValueError(f"Не найдено ни одной очереди для создания")
 
     try:
-        with RabbitMQClient(host=host, port=port, username=token, password=token) as client:
-            for queue_name in queue_names:
-                client.create_queue(queue_name, durable=True)
-    except (pika.exceptions.AMQPConnectionError, pika.exceptions.AMQPChannelError) as e:
-        raise ValueError(f"Ошибка при создании очередей: {e}")
+        if exchange_names:
+            with RabbitMQClient(host=host, port=port, username=token, password=token) as client:
+                for exchange_name in exchange_names:
+                    client.create_exchange(exchange_name, 'direct', durable=True)
+        if queue_names:
+            with RabbitMQClient(host=host, port=port, username=token, password=token) as client:
+                for queue_name in queue_names:
+                    client.create_queue(queue_name, durable=True)
+    except (pika.exceptions.AMQPConnectionError) as e:
+        print(f"Ошибка соединения с RabbitMQ при создании очередей: {e}")
